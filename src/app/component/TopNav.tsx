6d861953e6b77b985dcb52a91071eebf0ced0733
Aaron T <info@aarontan.co.nz>
2024-07-19 15:07:01 +1200

    WIP - Connect to database

---
+++ src/app/component/TopNav.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TopNav = () => {
+  return (
+    <div className='flex w-full items-center justify-between p-4 text-xl font-semibold border-b'>
+      <div>Gain Tracker</div>
+      <div>Sign In</div>
+
+    </div>
+  )
+}
+
+export default TopNav
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
-export default function HomePage() {
+export const dynamic = "force-dynamic";
+
+export default async function HomePage() {
+
+  const posts = await db.query.posts.findMany();
+
+  console.log(posts);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="flex flex-wrap">
       <h1>Gain Tacker</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+
+            <a>{post.name}</a>
+
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
