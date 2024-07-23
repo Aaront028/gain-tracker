d411bc77c90c1c97115ba458e4397076fa645dec
Aaron <95986810+Aaront028@users.noreply.github.com>
2024-07-23 15:47:50 +1200

    WIP - Restructured database and  added workouts dropdown menu (#4)

--- src/components/UserWorkout.tsx
+++ src/components/UserWorkout.tsx
@@ -38,6 +38,12 @@ interface WorkoutHistory {
   reps: number;
 }
 
+interface Exercise {
+  id: number;
+  name: string;
+  category: string;
+}
+
 interface UserWorkoutProps {
   workouts: Workout[];
   currentUserId: string | undefined;
