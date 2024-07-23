f699f6c29e4fd4fb136453c643e620f56e8cc83d
Aaron <95986810+Aaront028@users.noreply.github.com>
2024-07-24 00:19:14 +1200

    WIP - Create show_workout to allow show and hide stats (#8)

--- src/components/UserWorkout.tsx
+++ src/components/UserWorkout.tsx
@@ -38,12 +38,6 @@ interface WorkoutHistory {
   reps: number;
 }
 
-interface Exercise {
-  id: number;
-  name: string;
-  category: string;
-}
-
 interface UserWorkoutProps {
   workouts: Workout[];
   currentUserId: string | undefined;
